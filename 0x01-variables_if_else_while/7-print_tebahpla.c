a1c4fdd9628daf6b7d263c04ffe66baf1c9a635f
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-06-16 15:41:33 -0700

    Prints the lowercase alphabet in reverse.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - prints the lowercase alphabet in reverse.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
