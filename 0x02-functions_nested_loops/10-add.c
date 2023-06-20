33454aa90161dcea3d3247bd8eead24c444187d1
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-06-20 16:44:11 -0700

    Add two integrs.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* add - adds two integers and returns the result.
+* Return: return the result.
+* @a: the first input of the function add.
+* @b: the second input of the function add.
+*/
+
+
+int add(int a, int b)
+{
+	int ad;
+
+	ad = a + b;
+	return (ad);
+}
