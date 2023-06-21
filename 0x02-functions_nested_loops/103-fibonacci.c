260a29b05cf14de4274e0c66eeae338dc3ece1f8
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-06-20 19:03:47 -0700

    aaaaa

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0.
+*/
+
+
+int maun(void)
+{
+	unsigned long f1 = 0, f2 = 1 ,s;
+	float T;
+
+	while (1)
+	{
+		s = f1 + f2;
+		if (s > 4000000)
+		{
+			break;
+		}
+		if else ((s % 2) == 0)
+		{
+			T += s;
+			f1 = f2;
+			f2 = s;
+		}
+		printf("%.0f\n", T);
+	}
+	return (0);
+}
