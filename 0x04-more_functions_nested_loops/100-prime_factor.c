2ef5a2404a7edb2aaf2c76d3668ee67f602d4466
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-06-22 13:28:23 -0700

    Finds and prints the largest prime factor of the number 612852475143

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+* Prime_fac - finds and prints the largest prime factor of a number.
+* @n: the input of the function
+* Return: the largest prime factor.
+*/
+
+long int Prime_fac(long int n)
+{
+	long int larg_Fac;
+	long int i;
+
+	while (n % 2 == 0)
+	{
+		n /= 2;
+	}
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			larg_Fac = i;
+			n /= i;
+		}
+	}
+	if (n > 2)
+		larg_Fac = n;
+	return (larg_Fac);
+}
+
+
+/**
+ * main - Entry point.
+ * Return: Always 0 success
+ */
+
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int m = Prime_fac(n);
+
+	printf("The largest prime factor of %ld is %ld\n", n, m);
+	return (0);
+}
