7e493a9644281c335bc4723fbaa5965505af97bb
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-06-23 20:26:51 -0700

    change

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - print the character a
- * @a: the input of the function
+ * _putchar - print the input a
+ * @a: input of the function _putchar.
  * Return: (1) mean success
  */
+
 int _putchar(char a)
 {
 	return (write(1, &a, 1));
