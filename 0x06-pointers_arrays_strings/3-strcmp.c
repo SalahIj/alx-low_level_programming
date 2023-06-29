540ff59539764893114584d3ac5cf6917d27a077
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-06-29 08:50:57 -0700

    Function that compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, res = 0;
 
 	for (i = 0; s1[i] && s2[i]; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			res = s1[i] - s2[i];
+			break;
 		}
 	}
+	return ((int)res);
 }
