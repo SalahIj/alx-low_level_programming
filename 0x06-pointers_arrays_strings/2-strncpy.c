f6b5477527509bfc821702a2d0482c131f649501
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-06-29 07:42:46 -0700

    Write a function that copies a string.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strncpy -  coping two string with n bytes.
+* @dest: the first input.
+* @src: the second input.
+* @n:  bytes.
+* Return: the first input.
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int l;
+
+	for (l = 0; l < n && *(src + l) != '\0'; l++)
+	{
+		dest[l] = src[l];
+	}
+	while (l < n)
+	{
+		*(dest + l) = '\0';
+		l++;
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0.Task0:Write a function that concatenates two strings.
 1.Task1:Write a function that concatenates two strings with n bytes.
+2.Task2:Write a function that copies a string.
 
