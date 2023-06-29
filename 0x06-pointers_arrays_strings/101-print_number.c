18253bd04ef2f4b64aa88ab7006b0ff51b0d856e
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-06-29 14:10:53 -0700

    Printing numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,17 @@
 
 void print_number(int n)
 {
+	unsigned int p;
+
+	p = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		p = -n;
 	}
-	if ((n / 10)  > 0)
+	if (p / 10)
 	{
-		print_number(n / 10);
+		print_number(p / 10);
 	}
-	_putchar((n % 10)  + '0');
+	_putchar((p % 10)  + '0');
 }
