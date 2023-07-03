414d2f2a4fe5b5a538a51ebf3c6ae513bc767606
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-07-03 14:20:10 -0700

    Function that sets the value of a pointer to a char.

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* set_string - changing strings.
+* @s: the first input.
+* @to: the second input.
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
