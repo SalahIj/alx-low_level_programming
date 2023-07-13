1505f8c9b01d764f30bb0952df0a25bdcc1fb1c3
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-07-13 06:44:27 -0700

    Function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,49 +12,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int l_1, l_2, l = 0, j, ind = 0;
 	char *srt;
-	int l_1, l_2, i, ind;
 
-	l_1 = _strlen(s1);
-	l_2 = _strlen(s2);
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	srt = (char *)malloc(sizeof(char) * (l_1 + n + 1));
-	if (srt == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < l_1; i++)
-	{
-		srt[i] = s1[i];
-	}
+	for (l_1 = 0; s1[l_1]; l_1++)
+	;
+	for (l_2 = 0; s1[l_2]; l_2++)
+	;
 	if (n < l_2)
-	{
-		for (ind = 0; ind < n; ind++)
-		{
-			srt[i] = s2[ind];
-			i++;
-		}
-		srt[l_1 + n] = '\0';
-	}
+		l = l_1 + l_2;
 	else
+		l = l_1 + n;
+
+	srt = (char *)malloc(sizeof(srt) * n + 1);
+	if (srt == NULL)
+		return (NULL);
+	for (j = 0; j < l; j++)
 	{
-		for (ind = 0; ind < l_2; ind++)
-		{
-			srt[i] = s2[ind];
-			i++;
-		}
-		for (i = l_1 + l_2; i < l_1 + n; i++)
-		{
-			srt[i] = "";
-		}
-		srt[l_1 + n] = '\0';
+		if (j < l_1)
+			srt[j] = s1[j];
+
+		else
+			srt[j] = s2[ind++];
 	}
+	srt[l] = '\0';
 	return (srt);
 }
