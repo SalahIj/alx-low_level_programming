cede3e033a8fd3225783c1876a88b0efa7429526
SalahIj <ijbasalaheddine@gmail.com>
2023-07-27 16:18:19 +0000

    Task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
- *  * _strlen - calculiting.
- *   * @s: the input.
- *    * Return: the result.
-     */
-
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	if (s == NULL)
-		return (0);
-	while (*(s + i))
-		i++;
-	return (i);
-}
-
-/**
  *  * add_node - adding nodes
  *   * @head: the first input
  *    * @str: the second input
@@ -35,7 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (str != NULL)
 	{
 		current->str = strdup(str);
-		current->len = _strlen(current->str);
+		current->len = strlen(current->str);
 		if (current->str == NULL)
 		{
 			free(current);
