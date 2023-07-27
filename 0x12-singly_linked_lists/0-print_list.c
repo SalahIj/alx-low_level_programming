3e557e6ba6d66c8aab235411946cdfee4860bfd0
SalahIj <ijbasalaheddine@gmail.com>
2023-07-27 13:04:43 +0000

    Task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ *  * _strlen - calculiting.
+ *   * @s: the input.
+ *    * Return: the result.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (*(s + i))
+		i++;
+	return (i);
+}
+
+/**
  *  * print_list - printing
  *   * @h: the input of the function.
  *    * Return: the result.
