fa8ef6374fa5a3226c5464af04fdd336e8e788f4
SalahIj <ijbasalaheddine@gmail.com>
2023-08-03 19:17:24 +0000

    Task5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int X = n ^ m;
 	int counter = 0, i;
 
-	i = size;
+	i = size - 1;
 	while (i >= 0)
 	{
 		if ((X >> i) & 1)
