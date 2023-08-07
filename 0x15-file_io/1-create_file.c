d174fd412310757d2b84ac300396c08ebde2c0a1
SalahIj <ijbasalaheddine@gmail.com>
2023-08-07 17:03:00 +0000

    Task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
 	if (fp == -1)
 		return (-1);
 
