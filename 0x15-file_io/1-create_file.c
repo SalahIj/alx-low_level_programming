79be65bf4906fa5a9e1ad0ed7461b42e260e9c0f
SalahIj <ijbasalaheddine@gmail.com>
2023-08-07 16:43:28 +0000

    Task1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ *  _strlen - calculting lenght.
+ * @s: the input of the function.
+ * Return: the result.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * create_file - creating files.
+ * @filename: the first input.
+ * @text_content: the second input.
+ * Return: the result.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp = 0, lenght = 0, test = 0;
+
+	lenght = _strlen(text_content);
+	if (filename == NULL)
+		return (-1);
+
+	fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 200 | 400);
+	if (fp == -1)
+		return (-1);
+
+	if (lenght != 0)
+	{
+		test = write(fp, text_content, lenght);
+		if (test == -1)
+			return (-1);
+	}
+	return (1);
+}
