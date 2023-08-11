3966f1668ecf3244a4c5963d9fe132c54f234fc7
SalahIj <ijbasalaheddine@gmail.com>
2023-08-11 17:05:01 +0000

    Task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -22,20 +22,22 @@ void Close(int tab);
 
 void Check_ELF_files(unsigned char *magic_bytes)
 {
-	int i = 0, j;
-	char tab[4] = {127, 'F', 'L', 'E'};
+	int i = 0, j, k = 4;
+	char tab[4] = {127, 'E', 'L', 'F'};
 
 	while (i < 4)
 	{
 		for (j = 0; j < 4; j++)
 		{
 			if (magic_bytes[i] != tab[j])
-			{
-				fprintf(stderr, "Error : This File is not an ELF file\n");
-				exit(98);
-			}
+				k--;
 		}
 		i++;
+	}
+	if (k == 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+		exit(98);
 	}
 }
 
@@ -70,7 +72,7 @@ void Data(unsigned char *magic_bytes)
 {
 	char *tab2[3] = {"none", "2's complement, little endian",
 		"2's complement, big endian"};
-	int i = 0;
+	int i = 0, k = 0;
 
 	printf("  Data:                              ");
 	while (i < 3)
@@ -78,11 +80,13 @@ void Data(unsigned char *magic_bytes)
 		if (magic_bytes[EI_DATA] == i)
 		{
 			printf("%s\n", tab2[i]);
+			k = 1;
 			break;
 		}
 		i++;
 	}
-	printf("<unknown: %x>\n", magic_bytes[EI_DATA]);
+	if (k == 0)
+		printf("<unknown: %x>\n", magic_bytes[EI_DATA]);
 }
 
 /**
@@ -93,7 +97,7 @@ void Data(unsigned char *magic_bytes)
 void Class(unsigned char *magic_bytes)
 {
 	char *tab1[3] = {"none", "ELF32", "ELF64"};
-	int i;
+	int i, k = 0;
 
 	printf("  Class:                             ");
 	for (i = 0; i < 3; i++)
@@ -101,10 +105,12 @@ void Class(unsigned char *magic_bytes)
 		if (magic_bytes[EI_CLASS] == i)
 		{
 			printf("%s\n", tab1[i]);
+			k = 1;
 			break;
 		}
 	}
-	printf("<unknown: %x>\n", magic_bytes[EI_CLASS]);
+	if (k == 0)
+		printf("<unknown: %x>\n", magic_bytes[EI_CLASS]);
 }
 
 /**
@@ -134,11 +140,13 @@ void Version(unsigned char *magic_by)
 
 void Type(unsigned int e_type, unsigned char *tab)
 {
+	int k = 0;
 	unsigned int i;
 	char *tab1[5] = {"NONE (None)", "REL (Relocatable file)",
 		"EXEC (Executable file)", "DYN (Shared object file)",
 		"CORE (Core file)"};
 
+	printf("  Type:                              ");
 	if (tab[EI_DATA] == 2)
 		e_type >>= 8;
 
@@ -147,10 +155,12 @@ void Type(unsigned int e_type, unsigned char *tab)
 		if (e_type == i)
 		{
 			printf("%s\n", tab1[i]);
+			k = 1;
 			break;
 		}
 	}
-	printf("<unknown: %x>\n", e_type);
+	if (k == 0)
+		printf("<unknown: %x>\n", e_type);
 }
 
 /**
@@ -160,22 +170,25 @@ void Type(unsigned int e_type, unsigned char *tab)
 
 void OS_ABI(unsigned char *magic_by)
 {
-	int i;
+	int i, k = 0;
 	int tab1[11] = {0, 1, 2, 3, 6, 7, 8, 9, 10, 97, 255};
 	char *tab2[11] = {"UNIX - System V", "UNIX - HP-UX", "UNIX - NetBSD",
 		"UNIX - Linux", "UNIX - Solaris", "UNIX - AIX",
 		"UNIX - IRIX", "UNIX - FreeBSD", "UNIX - TRU64",
 		"ARM", "Standalone App"};
 
+	printf("  OS/ABI:                            ");
 	for (i = 0; i < 11; i++)
 	{
 		if (magic_by[EI_OSABI] == tab1[i])
 		{
 			printf("%s\n", tab2[i]);
+			k = 1;
 			break;
 		}
 	}
-	printf("<unknown: %x>\n", magic_by[EI_OSABI]);
+	if (k == 0)
+		printf("<unknown: %x>\n", magic_by[EI_OSABI]);
 }
 
 /**
@@ -185,7 +198,7 @@ void OS_ABI(unsigned char *magic_by)
 
 void ABI(unsigned char *tab)
 {
-	printf("  ABI Version:                       %d", tab[EI_ABIVERSION]);
+	printf("  ABI Version:                       %d\n", tab[EI_ABIVERSION]);
 }
 
 /**
