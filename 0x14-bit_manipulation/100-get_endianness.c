4aa7c705765dc5980c4912a2c21427ceb58cbf07
SalahIj <ijbasalaheddine@gmail.com>
2023-08-31 22:29:00 +0000

    Task6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - the name of the function.
+ * Return: The Result.
+ */
+
+int get_endianness(void)
+{
+	unsigned long int m;
+	char *string;
+
+	m = 1;
+	string = ((char *)&m);
+
+	return(*string);
+}
