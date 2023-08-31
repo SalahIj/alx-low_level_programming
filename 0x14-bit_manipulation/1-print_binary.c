afd50151c2a21efe8482488ba5ca9a450761c9a9
SalahIj <ijbasalaheddine@gmail.com>
2023-08-31 20:52:25 +0000

    Task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,31 @@
 #include "main.h"
 
 /**
- * print_binary - The name of the function.
- * @n: The entry.
- * Return: The result.
+ * print_binary - the name of the function
+ * @n: the entry
+ * Return: the result.
  */
 
 void print_binary(unsigned long int n)
 {
-	int i = 0, c = 0, j;
+	int contour = 0;
+	unsigned long int p;
 
-	if (n == 0)
+	for (p = n; p > 0; p >>= 1)
+	{
+		if (p & 1)
+		{
+			_putchar('1');
+			contour++;
+		}
+		else if (contour != 0)
+		{
+			_putchar('0');
+		}
+	}
+	if (contour == 0)
 	{
 		_putchar('0');
-		return;
-	}
-	while (n >> i)
-		i++;
-
-	for (j = i - 1; j >= 0; j--)
-	{
-		c = (n >> j) & 1;
-		_putchar(c + '0');
 	}
 }
+
