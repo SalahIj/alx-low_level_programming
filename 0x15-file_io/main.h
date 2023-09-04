de279681f2774a6b1d59fa0557a0d777c096929e
SalahIj <ijbasalaheddine@gmail.com>
2023-09-04 17:14:52 +0000

    Task0

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define BUFFER_SIZE 1024
+#define permission (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
