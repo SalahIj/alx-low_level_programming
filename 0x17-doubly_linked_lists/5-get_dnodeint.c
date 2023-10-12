428ac737b941b4f34749b2cb5c5188bce0907202
SalahIj <ijbasalaheddine@gmail.com>
2023-10-12 22:11:30 +0000

    Taks_5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,17 +9,15 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int j = 0;
-	dlistint_t *tp;
 
 	if (head == NULL)
 		return (NULL);
-	tp = head;
-	while (tp)
+	while (head)
 	{
-		tp = tp->next;
+		head = head->next;
 		j++;
 		if (j == index)
 			break;
 	}
-	return (tp);
+	return (head);
 }
