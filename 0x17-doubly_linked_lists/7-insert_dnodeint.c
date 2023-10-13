7a034f18d191e3abbe81dcb23e2678213f3f8151
SalahIj <ijbasalaheddine@gmail.com>
2023-10-13 00:54:38 +0000

    Task_7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,31 +7,34 @@
  * @n: the third input.
  * Return: The result.
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temoi, *new_node;
+	dlistint_t *tmp, *new_node, *next_node;
+	unsigned int pos;
 
+	if (*h == NULL)
+		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	temoi = *h;
-	for (; idx != 1; idx--)
-	{
-		temoi = temoi->next;
-		if (temoi == NULL)
-			return (NULL);
-	}
-	if (temoi->next == NULL)
-		return (add_dnodeint_end(h, n));
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
+	tmp = *h;
+	for (pos = 0; pos != idx - 1; pos++)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+	if (tmp->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	next_node = tmp->next;
+	tmp->next = new_node;
+	next_node->prev = new_node;
 	new_node->n = n;
-	new_node->prev = temoi;
-	new_node->next = temoi->next;
-	temoi->next->prev = new_node;
-	temoi->next = new_node;	
-	
+	new_node->prev = tmp;
+	new_node->next = next_node;
 	return (new_node);
 }
