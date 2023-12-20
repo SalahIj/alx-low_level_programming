389131281aa7355da813d4c54c5e71dab6f6846a
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-12-20 12:18:53 +0000

    Task_5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Imported modules """
+
+def island_perimeter(grid):
+    """ The function definition 
+    Args:
+        gris: the input
+    Return
+        the result
+    """
+    wid = len(grid[0])
+    hei = len(grid)
+    ed = 0
+    si = 0
+
+    for i in range(hei):
+        for j in range(wid):
+            if (grid[i][j] == 1):
+                si += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    ed += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    ed += 1
+    return (si * 4 - ed * 2)
