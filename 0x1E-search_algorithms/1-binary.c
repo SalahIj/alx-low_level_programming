79a9e1310ddcfe02182a96dcdf2093d052b01684
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2024-02-06 17:10:23 +0000

    Task_1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,63 +1,36 @@
 #include "search_algos.h"
 
 /**
- * print_array - prints the array of ints, separated with commas
- *
- * @array: a pointer to the array to print
- * @first: the first index to print
- * @last: the last index to print
+ * binary_search - The function name
+ * @array: The first input
+ * @size: The second input
+ * @value: The third input
+ * Return: The result
  */
-
-void print_array(int *array, size_t first, size_t last)
-{
-	size_t i;
-
-	printf("Searching in array: ");
-	for (i = first; i <= last; i++)
-	{
-		if (i != first)
-			printf(", ");
-		printf("%d", array[i]);
-	}
-	printf("\n");
-}
-
-/**
- * binary_search - searches for a value in an array of integers
- *
- * @array: a pointer to the first element of the array to search in
- * @size: the number of elements in array
- * @value: the value to search for
- *
- * Return: the index where value is located or -1 on failure or not found
- */
-
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i = 0, first = 0, last = size - 1;
+	size_t j = 0, left = 0, right = size - 1;
 
-	if (array)
+	if (array != NULL)
 	{
-		while (first <= last)
+		while (left <= right)
 		{
 			printf("Searching in array: ");
-			for (i = first; i <= last; i++)
+			for (j = left; j <= right; j++)
 			{
-				if (i != first)
+				if (j != left)
 					printf(", ");
-				printf("%d", array[i]);
+				printf("%d", array[j]);
 			}
 			printf("\n");
-			i = (first + last) / 2; /* Gets the middle value */
-
-			if (value > array[i]) /* Uses the right part of array */
-				first = i + 1;
-			else if (value < array[i]) /* Uses the left part of array */
-				last = i - 1;
+			j = (left + right) / 2;
+			if (value > array[i])
+				left = j + 1;
+			else if (value < array[i])
+				right = j - 1;
 			else
-				return (i);
+				return (j);
 		}
 	}
-
 	return (-1);
 }
