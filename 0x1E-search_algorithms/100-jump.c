8809f08bd4b53026633e04fc09271de99e8a4558
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2024-02-06 17:54:36 +0000

    Task_7

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,50 +1,30 @@
 #include "search_algos.h"
-#include <math.h>
-
-int min_value(int num1, int num2);
 
 /**
- * jump_search - Searches value in array of ints using the Jump search algo
- *
- * @array: Array to search
- *
- * @size: Size of the array
- *
- * @value: Value to search
- *
- * Return: First index where value is located or -1 for NULL array
+ * jump_search - The function name
+ * @array: the first input
+ * @size: The second input
+ * @value: The third input
+ * Return: Return the result
  */
-
 int jump_search(int *array, size_t size, int value)
 {
-	/* Define jump number by square root of size */
-	int jump = sqrt(size);
+	int step = sqrt(size);
 	int index = 0;
 
-	/* Check inputs */
 	if (!array)
-	{
 		return (-1);
-	}
 
-	/* Find block of array where value is found in jump_step */
 	while (index < (int)size && array[index] < value)
 	{
 		printf("Value checked array[%d] = [%d]\n", index, array[index]);
-		index += jump;
+		index += step;
 	}
-
-	/* Set jumpsize to index at beginning of array block to search*/
-	index -= jump;
-
-	printf("Value found between indexes [%d] and [%d]\n", index, index + jump);
-
-	/* Value found in block of size jump_step. Linearly find value */
-	while (index <= (index + jump) && index < (int)size)
+	index -= step;
+	printf("Value found between indexes [%d] and [%d]\n", index, index + step);
+	while (index <= (index + step) && index < (int)size)
 	{
 		printf("Value checked array[%d] = [%d]\n", index, array[index]);
-
-		/* Found return value at index */
 		if (array[index] == value)
 		{
 			return (index);
